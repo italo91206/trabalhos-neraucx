af4d7a6c22a99584d4f891b387a7729364232a52
Italo Ferreira <italoferreiradjk14@gmail.com>
2020-05-15 15:41:25 -0300

    Tradução arrumada + menu mobile

--- soul free travel/carrega-idioma.js
+++ soul free travel/carrega-idioma.js
@@ -1,16 +1,36 @@
 window.onload = function(){
-    var url = window.location.href;
-    var tem = url.includes("/en/");
+    var ToEnglish = jQuery(".st-list").children()[3];
+    var ToPortuguese = jQuery(".st-list").children()[4];
+    
+    jQuery(ToEnglish).addClass("toEnglish");
+    jQuery(ToPortuguese).addClass("toPortuguese");
 
-    // var elem_en = jQuery(".st-list").children()[4];
-    // var elem_pt = jQuery(".st-list").children()[5];
+    var atual = window.location.href;
+    var tem = atual.includes("/en/");
+    var final = "";
 
     if(tem){
+        // quero poder ir pra português
         jQuery(".st-list").children()[3].remove()
+        var partes = atual.split(".com.br/en/");
+        final = partes[0];
+        final = final.concat(".com.br/");
+        final = final.concat(partes[1]);
         this.console.log("Tinha!");
     }
     else{
+        // quero poder ir pra inglês
+        var partes = atual.split(".com.br/");
+        final = partes[0];
+        final = final.concat(".com.br/en/");
+        final = final.concat(partes[1]);
         jQuery(".st-list").children()[4].remove()
         this.console.log("Não tinha!");
     }
+    
+    var link = jQuery(".st-list").children()[3];
+    link = jQuery(link).children()[0];
+    jQuery(link).attr("href", final);
+
+    console.log(final);
 }
