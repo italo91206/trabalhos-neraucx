0bbed2bba7767dfd9ad53d89b6a18e00ce990aae
Italo Ferreira <italoferreiradjk14@gmail.com>
2020-06-02 14:29:00 -0300

    refatoração

--- soul free travel/scripts-geral.js
+++ soul free travel/scripts-geral.js
@@ -1,4 +1,9 @@
 window.onload = function(){
+
+    // ========================
+    // pra adicionar troca de idioma e consequentemente da moeda
+    // ========================
+
     var ToEnglish = jQuery(".st-list").children()[3];
     var ToPortuguese = jQuery(".st-list").children()[4];
     
@@ -37,5 +42,16 @@ window.onload = function(){
     link = jQuery(link).children()[0];
     jQuery(link).attr("href", param.href);
 
-    console.log(param.href);
+    // ========================
+    // pra adicionar os icones mobile
+    // ========================
+
+    var elem = jQuery("nav#st-main-menu ul#main-menu");
+    elem.append(`<li class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item mobile" style="color: grey" id="social-menu-list-item">`+
+        `<i class="fa fa-instagram"></i>`+
+        `<i class="fa fa-facebook-f"></i>`+
+        `<i class="fa fa-whatsapp"></i>`+
+        `<i class="fa fa-youtube"></i>`+
+        `<i class="fa fa-google-plus"></i><i class="fa fa-skype"></i>`+
+    `</li>`);
 }
