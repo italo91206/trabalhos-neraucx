dea04ad10a5b2e121ee0685fff7a43ea3dd77904
Italo Ferreira <italoferreiradjk14@gmail.com>
2020-07-28 03:50:09 -0300

    feito algumas coisas

---
+++ sneakers4all/src/components/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <main id="home">
+    <div id="opcoes" class="container">
+        <div class="flex">
+            <a href="/sneakers4all/API/controle.php?fn=forcar" target="_blank"><button>Forçar monitoramento</button></a>
+            <a href=""><button>Recarregar</button></a>
+        </div>
+    </div>
+
+    <div id="tabela-resultado" class="container">
+        <table>
+            <tr class="table-headers">
+                <th>Site</th>
+                <th>Produto</th>
+                <th>Situação</th>
+                <th>Preço</th>
+                <th>Link</th>
+                <th>Opções</th>
+            </tr>
+        </table>
+    </div>
+  </main>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      HTPP: axios.create({
+        baseURL: `http://localhost/sneakers4all/API/controle.php`,
+        headers: {
+          Authorization: 'Bearer {token}'
+        }
+      })
+    }
+  },
+  mounted(){
+      this.HTTP
+      .get('http://localhost/sneakers4all/API/controle.php?fn=listar')
+      .then(response => (this.info = response))
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
--- sneakers4all/src/main.js
+++ sneakers4all/src/main.js
@@ -1,7 +1,45 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import axios from 'axios';
+
+Vue.use(VueRouter)
+
 import App from './App.vue'
+import Home from './components/Home.vue'
+import About from './components/About.vue'
+
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    { path: '/', component: App },
+    { path: '/home', component: Home },
+    { path: '/about', component: About }
+  ]
+});
+
+export const HTTP = axios.create({
+  baseURL: `http://localhost/sneakers4all/API/controle.php`,
+  headers: {
+    Authorization: 'Bearer {token}'
+  }
+})
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+  axios,
+  router,
+  template: `
+  <div>
+    <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item"><router-link to="/home" class="nav-link">Home</router-link></li>
+          <li class="nav-item"><router-link to="/about" class="nav-link">About</router-link></li>
+          <li class="nav-item"><router-link to="/contact" class="nav-link">Contact</router-link></li>
+        </ul>
+      </div>
+    </nav>
+    <router-view class="view"></router-view>
+  </div>
+`
+}).$mount('#app')
