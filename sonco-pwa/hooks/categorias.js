ad5b26d82bd85ba759bd6ea49fcecd8bf393d4cd
Italo Ferreira <italoferreiradjk14@gmail.com>
2020-09-04 12:12:21 -0300

    arrumado sliders

--- sonco-pwa/hooks/categorias.js
+++ sonco-pwa/hooks/categorias.js
@@ -4,6 +4,7 @@
   BARRICADES CATEGORY 
   ===================
   */
+  // slider principal
   "--sc-slide__category-barricades": component => {
     component.$slide('#sc-slide__category-barricades', {
       slidesPerView: 1,
@@ -13,21 +14,33 @@
       }
     });
   },
-  /* THE 4 BANNERS FROM BARRICADES CATEGORY */
-  "--sc-slide__banners-barricades": component => {
-    component.$device.$isMobile('#sc-slide__banners-barricades', {
-      slidesPerView: 1,
-      centeredSlides: true,
+  // Slider do Free Shipping
+  "--sc-slide__products-barricades-01": component => {
+    component.$slide('#sc-slide__products-barricades-01', {
+      slidesPerView: window.innerWidth <= 1024 ? 1 : 'auto',
+      spaceBetween: window.innerWidth <= 1024 ? 0 : 30,
       navigation: {
-        nextEl: '.sc-slide__banners-barricades-next',
-        prevEl: '.sc-slide__banners-barricades-prev',
-      },
+        nextEl: '.sc-slide__products-barricades-01-next',
+        prevEl: '.sc-slide__products-barricades-01-prev',
+      }
+    });
+  },
+  // Slider do Hot Deals
+  "--sc-slide__products-barricades-02": component => {
+    component.$slide('#sc-slide__products-barricades-02', {
+      slidesPerView: window.innerWidth <= 1024 ? 1 : 'auto',
+      spaceBetween: window.innerWidth <= 1024 ? 0 : 30,
+      navigation: {
+        nextEl: '.sc-slide__products-barricades-02-next',
+        prevEl: '.sc-slide__products-barricades-02-prev',
+      }
     });
   },
   /* BARRICADES CAROUSEL 1 - PRODUCTS */
   "--sc-slide__barricades-products-01": component => {
     component.$slide('#sc-slide__barricades-products-01', {
       slidesPerView: window.innerWidth <= 1024 ? 1 : 'auto',
+      spaceBetween: window.innerWidth <= 1024 ? 0 : 30,
       navigation: {
         nextEl: '.sc-slide__barricades-products-01-next',
         prevEl: '.sc-slide__barricades-products-01-prev',
@@ -38,6 +51,7 @@
   "--sc-slide__barricades-products-02": component => {
     component.$slide('#sc-slide__barricades-products-02', {
       slidesPerView: window.innerWidth <= 1024 ? 1 : 'auto',
+      spaceBetween: window.innerWidth <= 1024 ? 0 : 30,
       navigation: {
         nextEl: '.sc-slide__barricades-products-02-next',
         prevEl: '.sc-slide__barricades-products-02-prev',
@@ -49,6 +63,7 @@
   CUSTOM SIGNS CATEGORY 
   ===================
   */
+  // Slider principal
   "--sc-slide__category-custom-signs": component => {
     component.$slide('#sc-slide__category-custom-signs', {
       slidesPerView: 1,
@@ -58,13 +73,36 @@
       }
     });
   },
-  // THE 4 BANNERS FROM CUSTOM SIGNS CATEGORY
-  "--sc-slide__banners-custom-signs": component => {
-    component.$slide('#sc-slide__banners-custom-signs', {
-      slidesPerView: 1,
+  // Best sellers
+  "--sc-slide__products-custom-signs-01": component => {
+    component.$slide('#sc-slide__products-custom-signs-01', {
+      slidesPerView: window.innerWidth <= 1024 ? 1 : 'auto',
+      spaceBetween: window.innerWidth <= 1024 ? 0 : 30,
       navigation: {
-        nextEl: '.sc-slide__banners-custom-signs-next',
-        prevEl: '.sc-slide__banners-custom-signs-prev',
+        nextEl: '.sc-slide__products-custom-signs-01-next',
+        prevEl: '.sc-slide__products-custom-signs-01-prev',
+      }
+    });
+  },
+  // Hot deals
+  "--sc-slide__products-custom-signs-02": component => {
+    component.$slide('#sc-slide__products-custom-signs-02', {
+      slidesPerView: window.innerWidth <= 1024 ? 1 : 'auto',
+      spaceBetween: window.innerWidth <= 1024 ? 0 : 30,
+      navigation: {
+        nextEl: '.sc-slide__products-custom-signs-02-next',
+        prevEl: '.sc-slide__products-custom-signs-02-prev',
+      }
+    });
+  },
+  // Free Shipping
+  "--sc-slide__products-custom-signs-03": component => {
+    component.$slide('#sc-slide__products-custom-signs-03', {
+      slidesPerView: window.innerWidth <= 1024 ? 1 : 'auto',
+      spaceBetween: window.innerWidth <= 1024 ? 0 : 30,
+      navigation: {
+        nextEl: '.sc-slide__products-custom-signs-03-next',
+        prevEl: '.sc-slide__products-custom-signs-03-prev',
       }
     });
   },
@@ -73,6 +111,7 @@
   FENCE SOLUTIONS CATEGORY 
   ===================
   */
+  // Slider principal
   "--sc-slide__category-fence-solutions": component => {
     component.$slide('#sc-slide__category-fence-solutions', {
       slidesPerView: 1,
@@ -82,39 +121,57 @@
       }
     });
   },
-  // products slider
+  // Hot Deals
+  "--sc-slide__products-fence-solutions-01": component => {
+    component.$slide('#sc-slide__products-fence-solutions-01', {
+      slidesPerView: window.innerWidth <= 1024 ? 1 : 'auto',
+      spaceBetween: window.innerWidth <= 1024 ? 0 : 30,
+      navigation: {
+        nextEl: '.sc-slide__products-fence-solutions-01-next',
+        prevEl: '.sc-slide__products-fence-solutions-01-prev',
+      }
+    });
+  },
+  // Custom Printed
   "--sc-slide__products-fence-solutions-02": component => {
     component.$slide('#sc-slide__products-fence-solutions-02', {
-      slidesPerView: 1,
+      slidesPerView: window.innerWidth <= 1024 ? 1 : 'auto',
+      spaceBetween: window.innerWidth <= 1024 ? 0 : 30,
       navigation: {
         nextEl: '.sc-slide__products-fence-solutions-02-next',
         prevEl: '.sc-slide__products-fence-solutions-02-prev',
       }
     });
   },
-  "--sc-slide__fence-solutions-products-03": component => {
-    component.$slide('#sc-slide__fence-solutions-products-03', {
-      slidesPerView: 1,
+  // Popular accessories
+  "--sc-slide__products-fence-solutions-03": component => {
+    component.$slide('#sc-slide__products-fence-solutions-03', {
+      slidesPerView: window.innerWidth <= 1024 ? 1 : 'auto',
+      spaceBetween: window.innerWidth <= 1024 ? 0 : 30,
       navigation: {
-        nextEl: '.sc-slide__fence-solutions-products-03-next',
-        prevEl: '.sc-slide__fence-solutions-products-03-prev',
+        nextEl: '.sc-slide__products-fence-solutions-03-next',
+        prevEl: '.sc-slide__products-fence-solutions-03-prev',
       }
     });
   },
-  "--sc-slide__banners-fence-solutions": component => {
-    component.$slide('#sc-slide__banners-fence-solutions', {
-      slidesPerView: 1,
+  // New Aged Products
+  "--sc-slide__products-fence-solutions-04": component => {
+    component.$slide('#sc-slide__products-fence-solutions-04', {
+      slidesPerView: window.innerWidth <= 1024 ? 1 : 'auto',
+      spaceBetween: window.innerWidth <= 1024 ? 0 : 30,
       navigation: {
-        nextEl: '.sc-slide__banners-fence-solutions-next',
-        prevEl: '.sc-slide__banners-fence-solutions-prev',
+        nextEl: '.sc-slide__products-fence-solutions-04-next',
+        prevEl: '.sc-slide__products-fence-solutions-04-prev',
       }
     });
   },
+
   /* 
   ===================
   STANCHIONS CATEGORY 
   ===================
   */
+  // Slider principal
   "--sc-slide__category-stanchions": component => {
     component.$slide('#sc-slide__category-stanchions', {
       slidesPerView: 1,
@@ -124,21 +181,47 @@
       }
     });
   },
+  // popular accessories
   "--sc-slide__category-stanchions-01": component => {
     component.$slide('#sc-slide__category-stanchions-01', {
-      slidesPerView: 1,
+      slidesPerView: window.innerWidth <= 1024 ? 1 : 'auto',
+      spaceBetween: window.innerWidth <= 1024 ? 0 : 30,
       navigation: {
         nextEl: '.sc-slide__category-stanchions-01-next',
         prevEl: '.sc-slide__category-stanchions-01-prev',
       }
     });
   },
-  "--sc-slide__banners-stanchions": component => {
-    component.$slide('#sc-slide__banners-stanchions', {
-      slidesPerView: 1,
+  // custom belts
+  "--sc-slide__category-stanchions-02": component => {
+    component.$slide('#sc-slide__category-stanchions-02', {
+      slidesPerView: window.innerWidth <= 1024 ? 1 : 'auto',
+      spaceBetween: window.innerWidth <= 1024 ? 0 : 30,
       navigation: {
-        nextEl: '.sc-slide__banners-stanchions-next',
-        prevEl: '.sc-slide__banners-stanchions-prev',
+        nextEl: '.sc-slide__category-stanchions-02-next',
+        prevEl: '.sc-slide__category-stanchions-02-prev',
+      }
+    });
+  },
+  // rope styles
+  "--sc-slide__category-stanchions-03": component => {
+    component.$slide('#sc-slide__category-stanchions-03', {
+      slidesPerView: window.innerWidth <= 1024 ? 1 : 'auto',
+      spaceBetween: window.innerWidth <= 1024 ? 0 : 30,
+      navigation: {
+        nextEl: '.sc-slide__category-stanchions-03-next',
+        prevEl: '.sc-slide__category-stanchions-03-prev',
+      }
+    });
+  },
+  // elegance line
+  "--sc-slide__category-stanchions-04": component => {
+    component.$slide('#sc-slide__category-stanchions-04', {
+      slidesPerView: window.innerWidth <= 1024 ? 1 : 'auto',
+      spaceBetween: window.innerWidth <= 1024 ? 0 : 30,
+      navigation: {
+        nextEl: '.sc-slide__category-stanchions-04-next',
+        prevEl: '.sc-slide__category-stanchions-04-prev',
       }
     });
   },
@@ -147,6 +230,7 @@
   TRAFFIC SAFETY CATEGORY 
   ===================
   */
+  // Slider principal
   "--sc-slide__category-traffic-safety": component => {
     component.$slide('#sc-slide__category-traffic-safety', {
       slidesPerView: 1,
@@ -156,24 +240,49 @@
       }
     });
   },
-  "--sc-slide__banners-traffic-safety": component => {
-    component.$slide('#sc-slide__banners-traffic-safety', {
-      slidesPerView: 1,
+  // barricades
+  "--sc-slide__category-traffic-saffety-01": component => {
+    component.$slide('#sc-slide__category-traffic-saffety-01', {
+      slidesPerView: window.innerWidth <= 1024 ? 1 : 'auto',
+      spaceBetween: window.innerWidth <= 1024 ? 0 : 30,
       navigation: {
-        nextEl: '.sc-slide__banners-traffic-safety-next',
-        prevEl: '.sc-slide__banners-traffic-safety-prev',
+        nextEl: '.sc-slide__category-traffic-saffety-01-next',
+        prevEl: '.sc-slide__category-traffic-saffety-01-prev',
       }
     });
   },
-  "--sc-slide__category-traffic-safety-02": component => {
-    component.$slide('#sc-slide__category-traffic-safety-02', {
-      slidesPerView: 1,
+  // popular accessories
+  "--sc-slide__category-traffic-saffety-02": component => {
+    component.$slide('#sc-slide__category-traffic-saffety-02', {
+      slidesPerView: window.innerWidth <= 1024 ? 1 : 'auto',
+      spaceBetween: window.innerWidth <= 1024 ? 0 : 30,
       navigation: {
-        nextEl: '.sc-slide__category-traffic-safety-02-next',
-        prevEl: '.sc-slide__category-traffic-safety-02-prev',
+        nextEl: '.sc-slide__category-traffic-saffety-02-next',
+        prevEl: '.sc-slide__category-traffic-saffety-02-prev',
       }
     });
-  }
-
+  },
+  // highway safety
+  "--sc-slide__category-traffic-saffety-03": component => {
+    component.$slide('#sc-slide__category-traffic-saffety-03', {
+      slidesPerView: window.innerWidth <= 1024 ? 1 : 'auto',
+      spaceBetween: window.innerWidth <= 1024 ? 0 : 30,
+      navigation: {
+        nextEl: '.sc-slide__category-traffic-saffety-03-next',
+        prevEl: '.sc-slide__category-traffic-saffety-03-prev',
+      }
+    });
+  },
+  // safety screens
+  "--sc-slide__category-traffic-saffety-04": component => {
+    component.$slide('#sc-slide__category-traffic-saffety-04', {
+      slidesPerView: window.innerWidth <= 1024 ? 1 : 'auto',
+      spaceBetween: window.innerWidth <= 1024 ? 0 : 30,
+      navigation: {
+        nextEl: '.sc-slide__category-traffic-saffety-04-next',
+        prevEl: '.sc-slide__category-traffic-saffety-04-prev',
+      }
+    });
+  },
   
 }
