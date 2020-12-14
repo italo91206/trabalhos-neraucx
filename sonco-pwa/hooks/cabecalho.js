c11ad4b95d159a30a55c4ffdb5b7dca606c4ee59
Italo Ferreira <italoferreiradjk14@gmail.com>
2020-12-14 10:27:16 -0300

    sync

--- sonco-pwa/hooks/cabecalho.js
+++ sonco-pwa/hooks/cabecalho.js
@@ -51,26 +51,46 @@
             submenu.style.width = "256px"
         })
 
-        var applicationsMenu = document.getElementById('sc-applications-menu')
-        var buttonShopBy = document.getElementsByClassName('sc-top-nav__item--shopby')[0];
-
-        buttonShopBy.addEventListener("mouseover", function(){
-            if(!applicationsMenu.classList.contains('ativo'))
-                applicationsMenu.classList.add('ativo');
-        })
+        var applicationsMenu = document.getElementById('sc-applications-menu');
+        var buttonShopBy = document.querySelector('.sc-top-nav__item--shopby');
 
         applicationsMenu.addEventListener("mouseleave", function(){
             applicationsMenu.classList.remove('ativo');
         })
-        
+
+        /**
+         * Especifico para Shop By Application 
+         * */
+        buttonShopBy.addEventListener("mouseover", function(){
+            if(!buttonShopBy.classList.contains('ativo'))
+                buttonShopBy.classList.add('ativo');
+        });
+
+        buttonShopBy.addEventListener("mouseleave", function(){
+            if(buttonShopBy.classList.contains('ativo'));
+                buttonShopBy.classList.remove('ativo');
+        });
+
+        document.querySelector('#sc-menu-container ul').addEventListener("click", () => {
+            if (window.innerWidth > 1024) {
+                document.querySelector('.sc-menu').classList.remove('ativo');
+                buttonShopBy.classList.remove('ativo');
+                applicationsMenu.classList.remove('ativo');
+            }
+        });
     },
     "--menuMobilePreparation": component => {
-        var menu = document.getElementById("sc-mobile-menu")
-        var items = menu.children
-        var submenu = document.getElementsByClassName("sc-submenu")
-        var botoesRetorno = document.getElementsByClassName("sc-top-nav__getback")
-        var subitem = document.getElementsByClassName('sc-top-nav__subitem')
+        let menu = document.getElementById("sc-mobile-menu")
+        let items = menu.children
+        let submenu = document.getElementsByClassName("sc-submenu")
+        let botoesRetorno = document.getElementsByClassName("sc-top-nav__getback")
+        let subitem = document.querySelectorAll('.sc-top-nav__subitem a')
+        /* vars two submenu */
+        let twoNivelSubmenu = document.getElementById("sc-submenu-three")
+        let items02 = twoNivelSubmenu.children
+        let submenutwo = document.getElementsByClassName("sc-submenu-two-list")
 
+        /* function define link clicked */
         function qualApertei(elem){
             var resultado = 0;
             for(var i=0; i< items.length && items[i] != elem; i++){
@@ -79,11 +99,13 @@
             return resultado;
         }
 
+        /* function open submenu */
         function abreSubMenu(num){
             menu.classList.add('sc-submenu-closed');
             submenu[num].classList.remove('sc-submenu-closed');
         }
 
+        /* function close submenu */
         function fechaSubMenu(){
             for(var i=0; i< items.length; i++){
                 if(!submenu[i].classList.contains('sc-submenu-closed'))
@@ -104,24 +126,61 @@
                 fechaSubMenu();
             })
         }
-
-        function zerarEstado(){
-            for(var i=0; i<botoesRetorno.length; i++)
-                botoesRetorno[i].click();
-            closeMenuMobile();
+        
+        /* SUBMENU 02 */
+        function elementClick(elem){
+            let resultado = 0;
+            for(let i=0; i< items02.length && items02[i] != elem; i++){
+                resultado++;
+            }
+            return resultado;
         }
+
+        function abreSubMenuTwo(num02){
+            twoNivelSubmenu.classList.add('sc-submenu-closed');
+            submenutwo[num02].classList.remove('sc-submenu-closed');
+        }
+
+        function fechaSubMenuTwo(){
+            for(let i=0; i< items02.length; i++){
+                if(!submenutwo[i].classList.contains('sc-submenu-closed'))
+                submenutwo[i].classList.add('sc-submenu-closed');
+            }
+            twoNivelSubmenu.classList.remove('sc-submenu-closed');
+        }
+
+        for (let i = 0; i < items02.length; i++) {
+            items02[i].addEventListener("click", function () {
+                let num02 = elementClick(this);
+                abreSubMenuTwo(num02 - 1);
+            });
+        }
+
+        for (let i = 0; i < botoesRetorno.length; i++) {
+            botoesRetorno[i].addEventListener("click", function () {
+                fechaSubMenuTwo();
+            })
+        }
+
+        // for(var i=0; i<subitem.length; i++){
+        //     if (!subitem[i].classList.contains("sc-top-nav__getback")) {
+        //         subitem[i].addEventListener("click", function(){
+        //                 closeMenuMobile();
+        //         })
+        //     }
+        // }
 
         for(var i=0; i<subitem.length; i++){
             subitem[i].addEventListener("click", function(){
-                zerarEstado();
+                    closeMenuMobile();
             })
         }
-        
     },
     "--ativadorTopBar": component =>{
         var about_us = document.getElementsByClassName('sc-header-contact__about')
         var top_bar = document.getElementById('sc-about-top');
-        var icone = document.getElementsByClassName('svg-inline--fa fa-angle-up fa-w-10');
+        var icone = document.getElementsByClassName('svg-inline--fa fa-angle-down fa-w-10');
+        //var linksSelect = document.querySelectorAll(".sc-about_box a");
 
         about_us = about_us[0];
 
@@ -135,6 +194,16 @@
                 top_bar.classList.remove('active');  
             }
         })
+
+        top_bar.addEventListener("click", () => {
+            top_bar.classList.remove("active");
+        })
+
+        // for(var i = 0; i < linksSelect.length; i++) {
+        //     linksSelect[i].addEventListener("click", () => {
+        //         top_bar.classList.remove("active");
+        //     })
+        // }
     },
     "--ajuste-menu-ourProducts": component =>{
         var buton = document.getElementsByClassName('sc-top-nav__item--categories')[0];
@@ -144,6 +213,11 @@
         buton.addEventListener("mouseover", function(){
             menu.classList.add("ativo");
         })
+
+        buton.addEventListener("mouseleave", function(){
+            menu.classList.remove("ativo");
+        })
+
         menu.addEventListener("mouseleave", function(){
             menu.classList.remove("ativo");
         })
@@ -151,5 +225,29 @@
             links[i].addEventListener("click", function(){
                 menu.classList.remove("ativo");
             })
+    },
+
+    "--close-menu-items": component => {
+        let menuNavHover = document.querySelectorAll('.sc-item-especifit');
+        let menuItem = document.querySelectorAll('.sc-items-menu-nav');
+        let subItemsList = document.querySelectorAll('.sc-sonco-header-list');
+
+        // Percorrer por todos os elementos selecionados
+        for(let i = 0; i < menuNavHover.length; i++) {
+            // Evento de hover para adicionar a class responsável por exibir o menu com as opções
+            menuNavHover[i].addEventListener("mouseover", function(){
+                menuItem[i].classList.add('ativo-menu-items');
+            });
+
+            // Evento responsável por remover a class responsável por exibir menu
+            menuItem[i].addEventListener("mouseleave", function(){
+                menuItem[i].classList.remove('ativo-menu-items');
+            });
+
+            // Evento de clique responsável por remover a class responsável por exibir menu
+            subItemsList[i].addEventListener("click", function(){
+                menuItem[i].classList.remove('ativo-menu-items');
+            });
+        }
     }
 }
